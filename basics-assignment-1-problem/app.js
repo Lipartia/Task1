b0180aae4cbb28776995a5155a9ab358c925f466
Lipartiaa <nika.lipartia@developers-alliance.com>
2025-04-03 17:28:43 +0400

    Task1 Dasrulebuli

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            name: "Nika",
+            age: 25,
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQybQ9296S3HQxCEmN2B7gK4a2H5hlUCBYKIA&s"
+        };
+    },
+    computed: {
+        ageIn5Years() {
+            return this.age + 5;
+        },
+        randomNumber() {
+            return Math.random().toFixed(2);
+        }
+    }
+}).mount("#app");
